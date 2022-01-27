5ccbf4eb4b4e2be2aa9d66e7bc6f1d967689a2b2
Alex <alexbyebye@hotmail.com>
2022-01-27 19:22:36 +0100

    mobile and desktop proyect finished

---
+++ README.md
@@ -0,0 +1,67 @@
+# Frontend Mentor - Rock, Paper, Scissors game solution
+
+:point_right: This is the [Rock, Paper, Scissors game](aqui va el reto de frontendmentor) challenge on Frontend Mentor.
+
+## Overview
+
+### Links
+
+:heavy_check_mark: My `Solution` URL : [Frontend Mentor Solution](Aqui va tu solucion de frontendmentor) </br>
+:heavy_check_mark: My `Live` Site URL : [GitHub Pages](Aqui va tu solucion de github pages)
+
+Give a ⭐️ if you like this project!
+
+### Screenshot
+
+![screenshot of the solution](/design/desktop-preview.jpg)
+
+### Rules
+
+If the player wins, they gain 1 point. If the computer wins, the player loses one point.
+
+#### Original
+
+- Paper beats Rock
+- Rock beats Scissors
+- Scissors beats Paper
+
+### Screenshot
+
+![screenshot of the solution](/design/bonus/desktop-step-1-bonus.jpg)
+
+#### Bonus
+
+- Scissors beats Paper
+- Paper beats Rock
+- Rock beats Lizard
+- Lizard beats Spock
+- Spock beats Scissors
+- Scissors beats Lizard
+- Paper beats Spock
+- Rock beats Scissors
+- Lizard beats Paper
+- Spock beats Rock
+
+Not sure what Rock, Paper, Scissors, Lizard, Spock is? [Check out this clip from The Big Bang Theory](https://www.youtube.com/watch?v=iSHPVCBsnLw).
+
+## My process
+
+### Built with :
+
+- `Mobile-first workflow`
+- [HTML5](https://developer.mozilla.org/en-US/docs/Web/HTML) - _Semantic HTML5 markup_
+- [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS) - _Cascading Style Sheets_
+- [BEM](http://getbem.com/) - _Block Element Modifier_
+- [Flexbox](https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Basic_Concepts_of_Flexbox)
+- [JavaScript](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+
+## Author
+
+:man: **_Alexcarmonadev_** :
+
+- LinkedIn - [Alejandro Carmona](https://www.linkedin.com/in/alejandro-carmona-522339228/)
+- GitHub - [@alexcarmonadev](https://github.com/alexcarmonadev)
+- Frontend Mentor - [@alexcarmonadev](https://www.frontendmentor.io/profile/alexcarmonadev)
+- Twitter - [@alexcarmonadev](https://twitter.com/alexcarmonadev)
+- Instagram - [@alexcarmonadev](https://instagram.com/alexcarmonadev)
+- Email - [Contact me](mailto:alexcarmonadev@gmail.com)
---
+++ js/scripts2.js
@@ -0,0 +1,108 @@
+/* ------------LAS CONSTANTES------------ */
+const scoreNumber = document.querySelector('.score-number');
+const btns = document.querySelector('.main__btns');
+const mainBtns = document.querySelectorAll('.main-btn');
+const mainShow = document.querySelector('.main__show');
+const showUser = document.querySelector('.show-user');
+const showHouse = document.querySelector('.show-house');
+const showUserBtn = mainShow.querySelector('.show-user-btn');
+const showHouseBtn = mainShow.querySelector('.show-house-btn');
+const showResult = document.querySelector('.show-result');
+const showPlayBtn = document.querySelector('.show-play-btn');
+const modalBox = document.querySelector('.modal-box');
+const modalBtn = document.querySelector('.modal-btn');
+const modalBtnClose = document.querySelector('.modal-btn-close');
+const options = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+const overlay = document.querySelector('.overlay');
+
+/* ------------LA MODAL------------ */
+modalBtn.addEventListener('click', () => {
+  overlay.style.display = 'block';
+  modalBox.style.display = 'block';
+});
+
+modalBtnClose.addEventListener('click', () => {
+  overlay.style.display = 'none';
+  modalBox.style.display = 'none';
+});
+
+/* ------------EL JUEGO------------ */
+const start = (e) => {
+  showUserBtn.classList.remove('rock', 'paper', 'scissors', 'lizard', 'spock');
+  showHouseBtn.classList.remove('rock', 'paper', 'scissors', 'lizard', 'spock');
+  showPlayBtn.style.display = 'none';
+  showResult.style.display = 'none';
+
+  const selectedUserBtn = e.target.dataset.btn;
+  const randomNumber = Math.floor(Math.random() * 5);
+  const selectedHouseBtn = options[randomNumber];
+
+  mainShow.style.display = 'block';
+  btns.style.display = 'none';
+  showUserBtn.classList.toggle(selectedUserBtn);
+  totalScore = 0;
+
+  winner = () => {
+    totalScore = parseInt(scoreNumber.innerHTML);
+    const showPoints = () => {
+      setTimeout(() => {
+        scoreNumber.innerHTML = totalScore;
+      }, 1000);
+    };
+
+    if (selectedUserBtn == selectedHouseBtn) {
+      showResult.textContent = 'DRAW';
+    } else if (
+      (selectedUserBtn == 'rock' &&
+        (selectedHouseBtn == 'spock' || selectedHouseBtn == 'paper')) ||
+      (selectedUserBtn == 'paper' &&
+        (selectedHouseBtn == 'scissors' || selectedHouseBtn == 'lizard')) ||
+      (selectedUserBtn == 'scissors' &&
+        (selectedHouseBtn == 'spock' || selectedHouseBtn == 'rock')) ||
+      (selectedUserBtn == 'lizard' &&
+        (selectedHouseBtn == 'scissors' || selectedHouseBtn == 'rock')) ||
+      (selectedUserBtn == 'spock' &&
+        (selectedHouseBtn == 'paper' || selectedHouseBtn == 'lizard'))
+    ) {
+      totalScore -= 1;
+      showResult.textContent = 'YOU LOSE';
+      setTimeout(() => {
+        showHouse.style.display = 'block';
+        showUser.style.display = 'none';
+      }, 1000);
+      showPoints();
+    } else {
+      totalScore += 1;
+      showResult.textContent = 'YOU WIN';
+      setTimeout(() => {
+        showUser.style.display = 'block';
+        showHouse.style.display = 'none';
+      }, 1000);
+      showPoints();
+    }
+  };
+
+  setTimeout(() => {
+    showHouseBtn.style.display = 'block';
+    showHouseBtn.classList.add(selectedHouseBtn);
+    setTimeout(() => {
+      showResult.style.display = 'block';
+      showPlayBtn.style.display = 'block';
+    }, 1000);
+  }, 1000);
+
+  setTimeout(() => {
+    winner();
+  }, 1000);
+};
+
+showPlayBtn.addEventListener('click', () => {
+  btns.style.display = 'block';
+  mainShow.style.display = 'none';
+  showUser.style.display = 'none';
+  showHouse.style.display = 'none';
+});
+
+for (let mainBtn of mainBtns) {
+  mainBtn.addEventListener('click', start);
+}
